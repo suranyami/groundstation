ccdac7e907441f87ed4e93a10bc859317022f42d
David Parry <david.parry@suranyami.com>
2019-09-14 19:41:42 +1000

    Add mavlink dependency

--- mix.exs
+++ mix.exs
@@ -33,18 +33,19 @@ defmodule GroundStation.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.9"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.0"},
-      {:phoenix_live_view, "~> 0.1.0"},
+      {:calendar, "~> 0.17.6"},
       {:ecto_sql, "~> 3.1"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:mavlink, github: "suranyami/elixir-mavlink"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:phoenix, "~> 1.4.9"},
+      {:phoenix_ecto, "~> 4.0"},
+      {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_live_view, "~> 0.1.0"},
+      {:phoenix_pubsub, "~> 1.1"},
       {:plug_cowboy, "~> 2.0"},
-      {:calendar, "~> 0.17.6"}
+      {:postgrex, ">= 0.0.0"}
     ]
   end
 
